ad2ac11e572b9dcdbd934427f236824c32f3cc50
Plotnyk <YeP2809889@>
2017-11-08 23:52:09 +0200

    Test2

---
+++ src/MainApp.java
@@ -0,0 +1,42 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    private Stage primaryStage;
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        authorization();
+
+    }
+
+    /**
+     * Створюємо метод для запуску авторизації при запуску програми
+     */
+    private void authorization() {
+        try {
+            primaryStage.hide();
+            Parent loader = new FXMLLoader().load(getClass().getResource("/gui/login/authorization.fxml"));
+            primaryStage.setTitle("Авторизация");
+            primaryStage.setResizable(false);
+
+            Scene scene = new Scene(loader);
+            scene.getStylesheets().add(MainApp.class.getResource("/css/authorization.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println("Ошибка запуска окна авторизации");
+        }
+
+    }
+}
+
---
+++ src/controller/login/ControllerAuthor.java
@@ -0,0 +1,24 @@
+package controller.login;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by Женя on 08.11.2017.
+ */
+public class ControllerAuthor {
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void onActionButtonOkClick() {
+
+    }
+
+    @FXML
+    private void onActionButtonCancelClick() {
+        System.exit(0);
+    }
+}
